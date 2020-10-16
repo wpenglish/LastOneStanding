94f36c6bdb1eeb6383ed7153c66b553f6f6689a4
wpenglish <wpenglish27@gmail.com>
2020-10-15 21:20:44 -0400

    Updates

---
+++ precache-manifest.e0117c1a927e5deff3199eef5de3b96b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59ed5a94a685d8a14b1371c34075e7ca",
+    "url": "/LastOneStanding/index.html"
+  },
+  {
+    "revision": "8bfc768a8e6d41f6e621",
+    "url": "/LastOneStanding/static/css/main.2a71b96f.chunk.css"
+  },
+  {
+    "revision": "28ca71289e67c7f5c791",
+    "url": "/LastOneStanding/static/js/2.d8f1bae1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/LastOneStanding/static/js/2.d8f1bae1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bfc768a8e6d41f6e621",
+    "url": "/LastOneStanding/static/js/main.7551a968.chunk.js"
+  },
+  {
+    "revision": "4645a37bffd8dc7444fe",
+    "url": "/LastOneStanding/static/js/runtime-main.3e4f5b44.js"
+  },
+  {
+    "revision": "fd32260c24f8dc7265e464a268fc1ec0",
+    "url": "/LastOneStanding/static/media/header2.fd32260c.png"
+  }
+]);
