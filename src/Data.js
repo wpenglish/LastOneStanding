15c8564d8de577e30edf00439e74d3c14e509d07
wpenglish <wpenglish27@gmail.com>
2020-11-02 11:49:17 -0500

    Added Rules

--- src/Data.js
+++ src/Data.js
@@ -34,7 +34,7 @@ function Data(){
             </div>
             <div className="rulesDiv">
                 <div className="rules"><h1>Rules</h1></div>
-
+                <div className="rules"><p>You will input a list. It can be anything, but is typically a list of you favorites things for a category. You will then input how many players there will be. Once you click Submit, the game will start.<br /><br />The list will be put into a "deck" and then the cards will split into pairs. One card will be shown or "face up" and the other card will not be shown or "face down".<br /><br />The way a turn works is one player will choose a pair for another player. The player whose didn't choose the pair will then get to decide whether they want to save the face up card or save the face down card. Which ever card they don't save will be killed. The game keeps going until there is only one card left. The final 2 cards will be taken to a vote by everyone. Each player gets to save one card that was killed but only immediately after it was killed.<br /><br />This game and rules were taken by Easy Allies and this video: https://www.youtube.com/watch?v=uB_pvw7FU6I. If you want a better explantion of the rules, watch the video.</p></div>
             </div>
         </div>
     )
